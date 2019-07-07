0c3c7f949ac11ddd841e64602584f9b490f71fd0
ywh <379801796@qq.com>
2019-07-07 15:48:13 +0800

    增加了数据模型

--- typeidea/__init__.py
+++ typeidea/__init__.py
@@ -3,3 +3,5 @@
 @Author  : jphy181202@gmail.com
 @Time    : 2019/7/7 11:27
 """
+import pymysql
+pymysql.install_as_MySQLdb()
--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ['ywh18.cn']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
